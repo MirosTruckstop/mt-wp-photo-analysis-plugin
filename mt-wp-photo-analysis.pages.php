3f1c76f43dcc3ee156090b9bd015630f4aae5322
Xennis <code@xennis.org>
2018-12-13 01:35:02 +0100

    Add custom role and cap

--- mt-wp-photo-analysis.pages.php
+++ mt-wp-photo-analysis.pages.php
@@ -1,13 +1,10 @@
 <?php
 use MT\PhotoAnalysis\OptionsPage;
 
-/**
- * Admin menu hook: Add pages to menu.
- */
-function mt_pa_admin_menu() {
-	add_options_page('Fotoanalyse', 'Fotoanalyse', 'activate_plugins', 'mt-photo-analysis', 'mt_pa_photo_analysis');
-}
-add_action('admin_menu', 'mt_pa_admin_menu');
+add_action('admin_menu', function() {
+	# Add pages to menue
+	add_options_page(__('Fotoanalyse'), __('Fotoanalyse'), 'activate_plugins', 'mt-photo-analysis', 'mt_pa_photo_analysis');
+});
 
 /**
  * Page photo search
--- mt-wp-photo-analysis.php
+++ mt-wp-photo-analysis.php
@@ -21,6 +21,24 @@ define('MT_PA_DIR', WP_PLUGIN_DIR.'/'.MT_PA_NAME);
  */
 define('MT_PA_DIR_SRC_PHP', MT_PA_DIR.'/src/php');
 
+
+register_activation_hook(__FILE__, function() {
+	# Register a custom role and cap for the REST API
+	add_role('mt_pa_editor', __('MT Photo Analysis Editor'));
+	$role = get_role('mt_pa_editor');
+	$role->add_cap('mt_pa_edit_texts');
+});
+
+register_deactivation_hook(__FILE__, function() {
+	# Remove the custom cap and role for the REST API
+	$role = get_role('mt_pa_editor');
+	if ($role) {
+		$role->remove_cap('mt_pa_edit_texts');
+		remove_role('mt_pa_editor');
+	}
+});
+
+
 require_once(MT_PA_DIR.'/vendor/autoload.php');
 require_once(MT_PA_DIR.'/mt-wp-photo-analysis.pages.php');
 require_once(MT_PA_DIR.'/mt-wp-photo-analysis.routing.php');
