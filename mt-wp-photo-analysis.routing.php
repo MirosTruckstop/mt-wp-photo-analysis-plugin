82d1f9ede1d9643ddfc0fb1ba8afc26f884fc89f
Xennis <code@xennis.org>
2018-12-13 01:35:16 +0100

    Add REST API endoint

--- mt-wp-photo-analysis.routing.php
+++ mt-wp-photo-analysis.routing.php
@@ -3,6 +3,20 @@ use MT\PhotoAnalysis\Api\Server;
 
 const API_QUERY_VAR = 'mt_pa_route';
 
+add_action('rest_api_init', function () {
+	register_rest_route( 'mt-wp-photo-analysis/v1', '/text', array(
+		'methods' => 'POST',
+		'callback' => 'mt_pa_callback_text',
+		'permission_callback' => function() {
+			return current_user_can('mt_pa_edit_texts');
+		}
+	));
+});
+
+function mt_pa_callback_text( $data ) {
+	return 'Hello World';
+}
+
 function mt_pa_add_api_route() {
 	global $wp;
 	$wp->add_query_var(API_QUERY_VAR);
