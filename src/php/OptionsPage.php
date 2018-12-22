87a35ee3b12e5bd605ca2fde9398a91e9b1ed545
Xennis <code@xennis.org>
2018-12-22 17:40:13 +0100

    Queue photo URL and ID

--- src/php/OptionsPage.php
+++ src/php/OptionsPage.php
@@ -9,36 +9,41 @@ class OptionsPage {
 	private function __sample_publish() {
 		$photos = $this->__getPhotos();
 		foreach ($photos as $photo) {
-			$this->publish_message(self::queue_topic, $photo->id);
+			$message = get_bloginfo('url').'/bilder/'.$photo->path;
+			$attributes = ['id' => $photo->id];
+			$this->publish_message(self::queue_topic, $message, $attributes);
 		}
 		return $photos;
 	}
 	
 	public static function __getPhotos($limit=1, $output_type='OBJECT') {
 		global $wpdb;
-		return $wpdb->get_results("SELECT id, date FROM wp_mt_photo ORDER BY date DESC LIMIT ${limit}", $output_type);
+		// TODO: Remove the WHERE condition
+		return $wpdb->get_results("SELECT id, path FROM wp_mt_photo WHERE id = 3908 ORDER BY date ASC LIMIT ${limit}", $output_type);
 	}
 	
 	/**
-	* Publishes a message for a Pub/Sub topic.
-	*
-	* @param string $projectId  The Google project ID.
-	* @param string $topicName  The Pub/Sub topic name.
-	* @param string $message  The message to publish.
+	 * Publishes a message for a Pub/Sub topic.
+	 *
+	 * @param string $topicName  The Pub/Sub topic name.
+	 * @param string $message  The message to publish.
+	 * @param array $attributes The attributes to publish
 	*/
-	private function publish_message($topicName, $message) {
+	private function publish_message($topicName, $message, $attributes) {
 		$pubsub = new PubSubClient([
-			#'projectId' => $projectId,
 			'keyFile' => json_decode(GCP_APPLICATION_KEY, true)
 		]);
 		$topic = $pubsub->topic($topicName);
-		$topic->publish(['data' => $message]);
+		$topic->publish([
+			'data' => $message,
+			'attributes' => $attributes
+		]);
 	}
 
 	public function outputContent() {
 	?>
 	<div class="wrap">
-	<h2><?php _e('Fotoanalyse');?> (<?php _e('Beta'); ?></h2>
+	<h2><?php _e('Fotoanalyse');?> (<?php _e('Beta'); ?>)</h2>
 		<p><?php _e('Hier gibt es nichts zu sehen, oder doch'); ?>: <?php echo count($this->__sample_publish()); ?>!</p>
 	</div>
 	<?php
